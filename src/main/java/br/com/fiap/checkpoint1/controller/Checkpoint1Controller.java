03a7bbfa8230392bd39c5d9693a9ff6c7cd4144d
Matheus Perestrelo (TS-SA) <matheus_perestrelo@trendmicro.com>
2024-03-13 14:34:36 -0300

    Application Properties

--- src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
+++ src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
@@ -1,7 +1,15 @@
 package br.com.fiap.checkpoint1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Checkpoint1Controller {
 
+    @GetMapping("/ping")
+    public String ping() { return "Pong";}
 
 
 }
