5f77104640a206654d35785e2ea2a80a3bd27e95
Matheus Perestrelo (TS-SA) <matheus_perestrelo@trendmicro.com>
2024-03-13 14:26:16 -0300

    Application Controller

---
+++ src/main/java/br/com/fiap/checkpoint1/Checkpoint1Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.checkpoint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Checkpoint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Checkpoint1Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/checkpoint1/controller/Checkpoint1Controller.java
@@ -0,0 +1,7 @@
+package br.com.fiap.checkpoint1.controller;
+
+public class Checkpoint1Controller {
+
+
+
+}
